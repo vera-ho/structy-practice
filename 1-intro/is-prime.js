2633058619034c0637bf91e26c0e31edcbfd10df
Vera Ho <vh71886@gmail.com>
2022-08-11 13:53:36 -0400

    is-prime

---
+++ 1-intro/is-prime.js
@@ -0,0 +1,14 @@
+const isPrime = (n) => {
+    if(n < 2) return false;
+    
+    for(let i = 2; i < n; i++) {
+        if(n % i === 0) return false;
+    }
+    
+    return true;
+};
+  
+module.exports = {
+    isPrime,
+};
+  
