d6ed69996ad92bf63a8b8f096e8f6f3965f54e99
Vera Ho <vh71886@gmail.com>
2022-08-12 14:11:05 -0400

    Fix return value

--- 2-array-and-string/pair-sum.js
+++ 2-array-and-string/pair-sum.js
@@ -6,7 +6,7 @@ const pairSum = (numbers, targetSum) => {
     
     for(let i = 0; i < numbers.length - 1; i++) {
         for(let j = i + 1; j < numbers.length; j++ ) {
-            if(numbers[i] + numbers[j] === targetSum) pairs.push(i, j)
+            if(numbers[i] + numbers[j] === targetSum) return [i, j]
         }
     }
     
