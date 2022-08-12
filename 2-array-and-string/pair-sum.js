7a600a8597019690d57863cc7e599fda73041f38
Vera Ho <vh71886@gmail.com>
2022-08-12 13:30:08 -0400

    pair-sum

---
+++ 2-array-and-string/pair-sum.js
@@ -0,0 +1,19 @@
+// Time complexity: O(n^2)
+// Space complexity: O(n)
+
+const pairSum = (numbers, targetSum) => {
+    const pairs = [];
+    
+    for(let i = 0; i < numbers.length - 1; i++) {
+        for(let j = i + 1; j < numbers.length; j++ ) {
+            if(numbers[i] + numbers[j] === targetSum) pairs.push(i, j)
+        }
+    }
+    
+    return pairs;
+};
+  
+module.exports = {
+    pairSum,
+};
+  
