b2267fd05ecd14050c9b5f0b1fcc2b34c35e45ff
Vera Ho <vh71886@gmail.com>
2022-08-12 11:26:12 -0400

    update uncompress

--- 2-array-and-string/uncompress.js
+++ 2-array-and-string/uncompress.js
@@ -1,35 +1,34 @@
 // Time complexity: O(n x m)
-// Space complexity: O(n)
+// Space complexity: O(1)
 
 const uncompress = (s) => {
-    let uncompressed = "";  
+    let uncompressed = [];  
     let i = j = 0;
     
     while(j < s.length) {
       
-      if(!isLetter(s[j])) {
-        j++;
-      } else {
-        let num = s.slice(i, j);
-        let letter = s[j];
-        
-        for(let i = 0; i < num; i++) {
-          uncompressed += letter;   // O(n) linear because JS strings are immutable and a whole new string is created
+        if(!isLetter(s[j])) {
+            j++;
+        } else {
+            let num = s.slice(i, j);
+            let letter = s[j];
+            
+            for(let i = 0; i < num; i++) {
+                uncompressed.push(letter);
+            }
+            
+            j++;
+            i = j;
         }
-        
-        j++;
-        i = j;
-      }
     }
     
-    return uncompressed;
-  };
+    return uncompressed.join("");
+};
   
-  const isLetter = char => {
+const isLetter = char => {
     return char.length === 1 && (char.toUpperCase() != char.toLowerCase()) 
-  }
-  
-  module.exports = {
+}
+
+module.exports = {
     uncompress,
-  };
-  
+};
