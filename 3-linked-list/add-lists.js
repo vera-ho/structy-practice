ace5a629957ceeac7dac34b527c4180d2ed1fc4d
Vera Ho <vh71886@gmail.com>
2022-08-18 11:00:55 -0400

    add-lists

--- 3-linked-list/add-lists.js
+++ 3-linked-list/add-lists.js
@@ -18,3 +18,31 @@ const addLists = (head1, head2, carry = 0) => {
     node.next = addLists(next1, next2, carry)
     return node;
 }
+
+// Time complexity: O(n) linear - O( max(n, m) )
+// Space complexity: O(n) linear - O( max(n, m) )
+// Iterative solution
+const addListsRecursive = (head1, head2) => {
+    let dummy = new Node(null);
+    let tail = dummy;
+    let current1 = head1;
+    let current2 = head2;
+    let carry = 0;
+    
+    while(current1 || current2 || carry) {
+      let val1 = current1 ? current1.val : 0;
+      let val2 = current2 ? current2.val : 0;
+      let sum = val1 + val2 + carry;
+      let digit = sum % 10;
+      let node = new Node(digit);
+      
+      carry = sum > 9 ? 1 : 0;
+      tail.next = node;
+      tail = tail.next;
+      
+      if(current1) current1 = current1.next;
+      if(current2) current2 = current2.next;
+    }
+    
+    return dummy.next;
+  };
