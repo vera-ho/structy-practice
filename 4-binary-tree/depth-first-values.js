b7c5c109891b13e906bec99e9b542ef0498b1a02
Vera Ho <vh71886@gmail.com>
2022-08-18 11:01:19 -0400

    depth-first-values

---
+++ 4-binary-tree/depth-first-values.js
@@ -0,0 +1,38 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+// Iterative solution
+const depthFirstValues = (root) => {
+    if(!root) return [];
+    
+    // Uses a stack -> array; only add/remove from one end
+    let stack = [root];
+    let values = [];
+    
+    while(stack.length > 0) {
+        let current = stack.pop();
+        if(current.right) stack.push(current.right);
+        if(current.left) stack.push(current.left);
+        values.push(current.val)
+    }
+    return values;  
+};
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+// Recursive solution
+const depthFirstValuesRecursive = (root) => {
+    if(!root) return [];
+    if(!root.left && !root.right) return [root.val];   // not necessary
+    
+    let left = depthFirstValues(root.left);
+    let right = depthFirstValues(root.right);
+    return [root.val].concat(left, right);
+};
