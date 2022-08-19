a0f36fd1a95e6d4046fde53b7c786b4de5918d63
Vera Ho <vh71886@gmail.com>
2022-08-19 12:13:16 -0400

    Another most-frequent-char solution

--- 2-array-and-string/most-frequent-char.js
+++ 2-array-and-string/most-frequent-char.js
@@ -1,6 +1,5 @@
 // Time complexity: O(n)
 // Space complexity: O(n) 
-
 const mostFrequentChar = (s) => {
     let counter = {}
     let mostFrequent = s[0];
@@ -14,13 +13,27 @@ const mostFrequentChar = (s) => {
         if(counter[mostFrequent] < counter[char]) mostFrequent = char
     }
     
-    return mostFrequent
+    return mostFrequent;
 };
   
 // console.log(mostFrequentChar("hello"))
 // console.log(mostFrequentChar("potato"))
+
   
-module.exports = {
-    mostFrequentChar,
+// Time complexity: O(n) linear
+// Space complexity: O(n) linear
+const mostFrequentChar2 = (s) => {
+    let counter = {}
+    let maxChar = s[0];
+    
+    for(let char of s) {
+        if(counter[char]) counter[char]++
+        else counter[char] = 1
+    }
+     
+    for(let char in counter) {
+        if(counter[char] > counter[maxChar]) maxChar = char
+    }
+     
+    return maxChar;
 };
-  
