f2ac57dac296e043eb4657d7c509873133c9fae0
Vera Ho <vh71886@gmail.com>
2022-08-19 11:29:13 -0400

    max-value additional solutions

--- 1-intro/max-value.js
+++ 1-intro/max-value.js
@@ -1,3 +1,5 @@
+// Time complexity: O(n) linear
+// Space complexity: O(1) constant
 const maxValue = (nums) => {
     let max = nums[0];
     nums.forEach( number => {
@@ -7,7 +9,24 @@ const maxValue = (nums) => {
     })
     return max;
 };
+
+// Time complexity: O(n^2) due to slice
+// Space complexity: O(n) because recursion (memory stack)
+const maxValueRecursive = (nums) => {
+    if(!nums) return -Infinity;
+    if(nums.length === 1) return nums[0];
+    
+    let max = maxValue(nums.slice(1));
+    if(nums[0] > max) max = nums[0]
+    return max;
+};
   
-module.exports = {
-    maxValue,
+// Time complexity: O(n) linear
+// Space complexity: O(1) constant
+const maxValue2 = (nums) => {
+  let max = -Infinity;
+  for(let num of nums) {
+    if(num > max) max = num;
+  }
+  return max;
 };
