b9a10eb6ed53fe9da226988c67b0516ef5bbc613
Vera Ho <vh71886@gmail.com>
2022-08-20 19:39:03 -0400

    Add notes to is-prime

--- 1-intro/is-prime.js
+++ 1-intro/is-prime.js
@@ -1,3 +1,10 @@
+// Time complexity: O(n) linear
+// Space complexity: O(1) constant
+/** Approach:
+ *      - Only numbers >= 2 can be a prime number if they have no factors other than 1 and n
+ *      - Loop through numbers from 2 to n - 1
+ *      - If any factors are round, it is not a prime number
+ */
 const isPrime = (n) => {
     if(n < 2) return false;
     
@@ -6,9 +13,4 @@ const isPrime = (n) => {
     }
     
     return true;
-};
-  
-module.exports = {
-    isPrime,
-};
-  
+}
