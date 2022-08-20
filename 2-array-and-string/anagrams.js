e369d45d4ad22699d52fcab4f9c7d1eddf5ec300
Vera Ho <vh71886@gmail.com>
2022-08-20 19:49:33 -0400

    Another method anagrams

--- 2-array-and-string/anagrams.js
+++ 2-array-and-string/anagrams.js
@@ -11,16 +11,26 @@ const anagrams = (s1, s2) => {
     if(s1.length != s2.length) return false;
     
     let ctr = {};
-  
-    s1.split("").forEach( char => {
+
+    for(let char of s1) {
         if(ctr[char]) ctr[char]++
         else ctr[char] = 1
-    })
-    
-    s2.split("").forEach( char => {
+    }
+      
+    for(let char of s2) {
         if(ctr[char]) ctr[char]--
         else ctr[char] = -1
-    })
+    }
+    
+    // s1.split("").forEach( char => {
+    //     if(ctr[char]) ctr[char]++
+    //     else ctr[char] = 1
+    // })
+    
+    // s2.split("").forEach( char => {
+    //     if(ctr[char]) ctr[char]--
+    //     else ctr[char] = -1
+    // })
     
     return !Object.values(ctr).some( value => value !== 0)
 };
