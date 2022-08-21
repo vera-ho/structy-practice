8ae47ab21f47e4298c09174dd2bf0056f40f2b84
Vera Ho <vh71886@gmail.com>
2022-08-20 20:17:31 -0400

    Add note compress

--- 2-array-and-string/compress.js
+++ 2-array-and-string/compress.js
@@ -1,6 +1,15 @@
+/** Approach
+ *      - Count and track consecutive characters
+ *      - If character changes, save count if > 1 and character, 
+ *      - or just the char if count = 1
+ *      - Count of consecutive characters is also the index of new char minus index of first of prev character
+ *          - ffddddeejk
+ *          - 0123456789
+ *          - (6-2) = 4 consecutive d's    
+ */
+
 // Time complexity: O(n)
 // Space complexity: O(n)
-
 const compress = (s) => {
     let result = [];
     let prev = curr = 0;
