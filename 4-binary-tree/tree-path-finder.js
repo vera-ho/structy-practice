8fb1f2f32b3d2234ec6bc1d93d6e2b8de30b51b9
Vera Ho <vh71886@gmail.com>
2022-08-21 20:41:46 -0400

    Add O(n) solution tree-path-finder

--- 4-binary-tree/tree-path-finder.js
+++ 4-binary-tree/tree-path-finder.js
@@ -1,4 +1,4 @@
-// Time complexity: O(n) linear? - is concat O(n) or O(1)??
+// Time complexity: O(n) linear? - is concat O(n) or O(1)?? Note: Structy says [root.val, ...leftPath] is O(n) on its own; totoal O(n^2)
 // Space complexity: O(n) linear
 // Recursive DFS solution
 const pathFinder = (root, target) => {
@@ -14,3 +14,32 @@ const pathFinder = (root, target) => {
     return null;
 };
 
+
+// Time complexity: O(n) linear
+// Space complexity: O(n) linear
+// Recursive DFS solution that is O(n) using .push() and .reverse() and a helper function
+const pathFinderWithPush = (root, target) => {
+  let arr = pathFinderHelp(root, target);
+  if(!arr) return null
+  else return arr.reverse();
+}
+
+const pathFinderHelp = (root, target) => {
+    if(!root) return null;
+    if(root.val === target) return [root.val];
+    
+    let leftPath = pathFinderHelp(root.left, target);
+    if(leftPath) {
+      leftPath.push(root.val);
+      return leftPath
+    }
+        
+    let rightPath = pathFinderHelp(root.right, target);
+    if(rightPath) {
+      rightPath.push(root.val);
+      return rightPath;
+    }
+  
+    return null;
+  };
+  
