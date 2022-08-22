5f29cc75012e786fc8b37e69697a9c58998c5bec
Vera Ho <vh71886@gmail.com>
2022-08-22 15:01:36 -0400

    Approach all-tree-paths

--- 4-binary-tree/all-tree-paths.js
+++ 4-binary-tree/all-tree-paths.js
@@ -1,3 +1,12 @@
+/** Approach
+ *      - Base cases:
+ *          * root is null, return empty array
+ *          * root has no children, return 2D array with root value
+ *      - Find paths of children nodes
+ *      - Concatenate paths together
+ *      - Add current node value to each subarray 
+ */
+
 // Time complexity: O(n) linear
 // Space complexity: O(n) linear
 // DFS Recursive solution
