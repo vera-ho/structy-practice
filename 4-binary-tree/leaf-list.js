42c287317e1a7e9c281a301c94b3b2a64f55d883
Vera Ho <vh71886@gmail.com>
2022-08-23 15:34:04 -0400

    Faster leaf-list

--- 4-binary-tree/leaf-list.js
+++ 4-binary-tree/leaf-list.js
@@ -17,11 +17,27 @@ const leafList = (root) => {
     return leaves;
 };
 
-// Time complexity: O(n) linear
+// Time complexity: O(n^2) quadratic - spread operator is O(n)
 // Space complexity: O(n) linear
 // Recursive DFS solution
 const leafListRecursive = (root) => {
     if(!root) return [];
     if(!root.left && !root.right) return [root.val]
     return [...leafList(root.left), ...leafList(root.right)]
+};
+
+// // Time complexity: O(n) linear
+// // Space complexity: O(n) linear
+// // Recursive DFS solution
+const leafListRecursive2 = (root) => {
+    const leaves = [];
+    const leafFinder = (root, leaves) => {
+        if(!root) return;
+        if(!root.left && !root.right) leaves.push(root.val);
+        if(root.left) leafFinder(root.left, leaves);
+        if(root.right) leafFinder(root.right, leaves);
+    }
+    
+    leafFinder(root, leaves);
+    return leaves;
 };
