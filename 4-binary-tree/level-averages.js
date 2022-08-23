83e2d7859fd23247d607d6f4d2a5fd7ef68df8d0
Vera Ho <vh71886@gmail.com>
2022-08-23 13:52:53 -0400

    fix level-averages time complexity note

--- 4-binary-tree/level-averages.js
+++ 4-binary-tree/level-averages.js
@@ -1,4 +1,4 @@
-// Time complexity: O(n^2) quadratic - Average calculation nested loop
+// Time complexity: O(n) linear
 // Space complexity: O(n) linear
 // Iterative DFS solution
 const levelAverages = (root) => {
@@ -23,6 +23,34 @@ const levelAverages = (root) => {
         let sum = level.reduce( (a, b) => a + b, 0);
         averages.push(sum / level.length)
     }
+    
+    return averages;
+};
+
+// Time complexity: O(n) linear
+// Space complexity: O(n) linear
+// Iterative DFS solution
+const levelAverages2 = (root) => {
+    if(!root) return [];
+    
+    const treeLevelValues = [];
+    const stack = [ { node: root, level: 0 } ];
+    
+    // Get all level values first
+    while(stack.length > 0) {
+        let { node, level } = stack.pop();
+        if(node.right) stack.push({ node: node.right, level: level + 1});
+        if(node.left) stack.push({ node: node.left, level: level + 1});
+        
+        if(treeLevelValues[level]) treeLevelValues[level].push(node.val);
+        else treeLevelValues[level] = [node.val];
+    }
+  
+    // Then calculate averages
+    const averages = treeLevelValues.map( level => {
+        let sum = level.reduce( (a, b) => a + b, 0);
+        return sum / level.length;
+    })
     
     return averages;
 };
