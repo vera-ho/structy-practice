2a3e145bb128cc88bfb05f290cfd3f43afee0ba3
Vera Ho <vh71886@gmail.com>
2022-08-26 14:20:59 -0400

    has-path BFS

--- 5-graph/has-path.js
+++ 5-graph/has-path.js
@@ -16,8 +16,8 @@ const hasPath = (graph, src, dst) => {
     return false;
 };
 
-// Time complexity: 
-// Space complexity: 
+// Time complexity: O(e) linear
+// Space complexity: O(n) linear
 // Recursive DFS solution
 const hasPathRecursiveDFS = (graph, src, dst) => {
     if(src === dst) return true;
@@ -29,3 +29,21 @@ const hasPathRecursiveDFS = (graph, src, dst) => {
     
     return false;
 };
+
+// Time complexity: O(e) linear
+// Space complexity: O(n) linear
+// BFS solution
+const hasPathBFS = (graph, src, dst) => {
+    let queue = [ src ];
+    
+    while(queue.length > 0) {
+      let current = queue.shift();
+      if(current === dst) return true;
+      
+      for(let node of graph[current]) {
+        queue.push(node)
+      }
+    }
+    
+    return false;
+  };
