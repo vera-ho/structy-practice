034cbaf36f3180d683609660b59746b938168894
Vera Ho <vh71886@gmail.com>
2022-09-04 16:11:02 -0400

    Add node tracking to longest-path

--- 5-graph/longest-path.js
+++ 5-graph/longest-path.js
@@ -22,3 +22,35 @@ const traverseNode = (graph, node, pathLengths) => {
     
     return tempLength + 1;
 }
+
+// Time complexity: O(e) linear
+// Space complexity: O(n) linear
+const longestPathTracked = (graph) => {
+    // Track visited nodes, prevent redundant traversals
+    let pathLengths = {};
+    
+    // Find terminal nodes
+    for(let node in graph) {
+        if(graph[node].length === 0) pathLengths[node] = 0;
+    }
+    
+    // Calculate lengths - DFS recursive
+    for(let node in graph) {
+        traverseNode(graph, node, pathLengths);
+    }
+    
+    return Math.max(...Object.values(pathLengths));
+  };
+
+  const traverseNodeTracked = (graph, node, pathLengths) => {
+    if(node in pathLengths) return pathLengths[node];
+    
+    let max = 0;
+    for(let neighbor of graph[node]) {
+        let length = traverseNode(graph, neighbor, pathLengths);
+        if(length > max) max = length;
+    }
+    
+    pathLengths[node] = max + 1;
+    return pathLengths[node];
+}
