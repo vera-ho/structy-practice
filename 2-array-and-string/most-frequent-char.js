6a50aafb1adcdd7cd6b918d6479f56d35a7ee80a
Vera Ho <vh71886@gmail.com>
2022-09-05 13:30:17 -0400

    Redo most-frequent-char

--- 2-array-and-string/most-frequent-char.js
+++ 2-array-and-string/most-frequent-char.js
@@ -37,3 +37,23 @@ const mostFrequentChar2 = (s) => {
      
     return maxChar;
 };
+
+// Time complexity: O(n) linear
+// Space complexity: O(n) linear
+const mostFrequentChar3 = (s) => {
+    let counter = {};
+    
+    // Loop through string and count occurrences of characters
+    for(let char of s) {
+        if(!counter[char]) counter[char] = 0;
+        counter[char] += 1;
+    }
+    
+    // Loop through string and compare counts of each char
+    let mostChar = s[0];
+    for(let char of s) {
+        if(counter[char] > counter[mostChar]) mostChar = char;
+    }
+    
+    return mostChar;
+}
