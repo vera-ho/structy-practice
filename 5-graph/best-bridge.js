1d80340e9e51c899524835ee3aed6244c492898b
Vera Ho <vh71886@gmail.com>
2022-09-04 20:43:52 -0400

    clean up best-bridge

--- 5-graph/best-bridge.js
+++ 5-graph/best-bridge.js
@@ -1,13 +1,15 @@
-// Time complexity: 
-// Space complexity: 
+// Time complexity: O(n x m) linear
+// Space complexity: O(n x m) linear
 const bestBridge = (grid) => {
     const visited = new Set();
     
     for(let row = 0; row < grid.length; row++) {
         for(let col = 0; col < grid[row].length; col++) {
             let pos = row + " " + col;
+          
             if(grid[row][col] === 'L' && !visited.has(pos)) {
                 visited.add(pos);
+                
                 let islandMap = traverseIsland(grid, row, col, visited);
                 let bridgeLength = findShortestBridge(grid, islandMap, visited);
                 return bridgeLength;
@@ -24,15 +26,12 @@ const findShortestBridge = (grid, map, visited) => {
         let [row, col, steps] = queue.shift();
         if(grid[row][col] === 'L' && steps) return steps - 1;
         
-        for(let i = 0; i < dirs.length; i++) {
-            let [x, y] = dirs[i];
-            let nextRow = row + x;
-            let nextCol = col + y;
+        for(let dir of dirs) {
+            let [dr, dc] = dir;
+            let [nextRow, nextCol] = [row + dr, col + dc];
             let nextPos = nextRow + " " + nextCol;
-            let inBoundsRow = nextRow >= 0 && nextRow < grid.length;
-            let inBoundsCol = nextCol >= 0 && nextCol < grid[0].length;
-    
-            if(inBoundsCol && inBoundsRow && !visited.has(nextPos)) {
+
+            if(inBounds(nextRow, nextCol, grid) && !visited.has(nextPos)) {
                 queue.push([nextRow, nextCol, steps + 1]);
                 visited.add(nextPos);
             }
@@ -43,27 +42,29 @@ const findShortestBridge = (grid, map, visited) => {
 const traverseIsland = (grid, row, col, visited) => {
     const dirs = [ [0, 1], [1, 0], [-1, 0], [0, -1] ];
     const queue = [ [row, col] ];
-    const map = [ [row, col, 0] ];
+    const island = [ [row, col, 0] ];
     
     while(queue.length) {
         let [row, col] = queue.shift();
-        
-        for(let i = 0; i < dirs.length; i++) {
-            let [x, y] = dirs[i];
-            let nextRow = row + x;
-            let nextCol = col + y;
+      
+        for(let dir of dirs) {
+            let [dr, dc] = dir;
+            let [nextRow, nextCol] = [row + dr, col + dc];
             let nextPos = nextRow + " " + nextCol;
-            let inBoundsRow = nextRow >= 0 && nextRow < grid.length;
-            let inBoundsCol = nextCol >= 0 && nextCol < grid[0].length;
-    
-            if(inBoundsCol && inBoundsRow && 
-            grid[nextRow][nextCol] === 'L' && !visited.has(nextPos)) {
-                queue.push([nextRow, nextCol]);
-                map.push([nextRow, nextCol, 0]);
+
+            if(inBounds(nextRow, nextCol, grid) && grid[nextRow][nextCol] === 'L' && !visited.has(nextPos)) {
                 visited.add(nextPos);
+                queue.push([nextRow, nextCol]);
+                island.push([nextRow, nextCol, 0]);
             }
         }
     }
     
-    return map;
+    return island;
 };
+
+const inBounds = (row, col, grid) => {
+    const inBoundsRow = row >= 0 && row < grid.length;
+    const inBoundsCol = col >= 0 && col < grid[0].length;
+    return inBoundsRow && inBoundsCol;
+}
