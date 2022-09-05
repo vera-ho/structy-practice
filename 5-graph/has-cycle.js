c06c2782c0bd92f7b8ef05341faa8044b7545e49
Vera Ho <vh71886@gmail.com>
2022-09-04 22:25:48 -0400

    has-status

---
+++ 5-graph/has-cycle.js
@@ -0,0 +1,28 @@
+const hasCycle = (graph) => {
+    const visiting = new Set();
+    const visited = new Set();
+    
+    for(let node in graph) {
+        if(!visited.has(node)) {
+            let cycle = detectCycle(graph, node, visiting, visited);
+            if(cycle) return true;
+        }
+    }
+    
+    return false;
+};
+
+const detectCycle = (graph, node, visiting, visited) => {
+    if(visiting.has(node)) return true;
+    if(visited.has(node)) return false;
+    visiting.add(node);
+    
+    for(let neighbor of graph[node]) {
+        let cycle = detectCycle(graph, neighbor, visiting, visited);  
+        if(cycle) return true;
+    }
+    
+    visiting.delete(node);
+    visited.add(node);
+    return false;
+};
