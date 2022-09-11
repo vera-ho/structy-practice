d6d5610add34c4291ce4f4d1b0404bdd09b0d8b5
Vera Ho <vh71886@gmail.com>
2022-09-10 21:45:12 -0400

    better fib

--- 6-dynamic-programming/fib.js
+++ 6-dynamic-programming/fib.js
@@ -17,3 +17,17 @@ const fib = (n, fibValues = {}) => {
     fibValues[n] = n1Fib + n2Fib;
     return fibValues[n];  
 };
+
+// Time complexity: O(n) linear
+// Space complexity: O(n) linear
+const fibBetter = (n, fibValues = {}) => {
+    if(n === 0) return 0;
+    if(n === 1) return 1;
+    if(n in fibValues) return fibValues[n];
+    
+    let n1Fib = fib(n - 1, fibValues);
+    let n2Fib = fib(n - 2, fibValues);
+    
+    fibValues[n] = n1Fib + n2Fib;
+    return fibValues[n];  
+};
