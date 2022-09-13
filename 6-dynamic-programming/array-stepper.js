09aa32d01074bfa44173bcc919184ffd4af3be74
Vera Ho <vh71886@gmail.com>
2022-09-13 14:25:58 -0400

    array-stepper

---
+++ 6-dynamic-programming/array-stepper.js
@@ -0,0 +1,17 @@
+// Time complexity: O(n^2) polynomial/quadratic - If array and its values are always one step short, need to look through all positions
+// Space complexity: O(n)
+const arrayStepper = (nums, index = 0, steps = {}) => {
+    if(index === nums.length - 1) return true;
+    if(nums[0] === 0 || index >= nums.length) return false;
+    if(index in steps) return steps[index];
+    
+    let num = nums[index];
+    let possible = false;
+    for(let i = 1; i <= num; i++) {
+        possible = possible || arrayStepper(nums, index + i, steps);
+        if(possible) break;
+    }
+    
+    steps[index] = possible;
+    return possible;
+};
