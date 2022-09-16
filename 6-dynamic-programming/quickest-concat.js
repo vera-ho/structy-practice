6877639d125efbdc4f4246893c8d94df1cdff80d
Vera Ho <vh71886@gmail.com>
2022-09-16 12:44:34 -0400

    quickest-concat

---
+++ 6-dynamic-programming/quickest-concat.js
@@ -0,0 +1,22 @@
+// Time complexity: O(s x w)
+// Space complexity: O(s)
+const quickestConcat = (s, words) => {
+    let minWords = concatCounter(s, words);
+    return minWords === Infinity ? -1 : minWords;
+};
+  
+  const concatCounter = (s, words, dictionary = {}) => {
+    if(!s.length) return 0;
+    if(s in dictionary) return dictionary[s];
+  
+    let minWords = Infinity;
+    for(let word of words) {
+        if(s.startsWith(word)) {
+            let wordCt = concatCounter(s.slice(word.length), words, dictionary);
+            minWords = Math.min(minWords, wordCt);
+        }
+    }
+  
+    dictionary[s] = minWords + 1;
+    return dictionary[s];
+}
