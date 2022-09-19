c726c145a83775e8516076fdcb9f2d55b40741e1
Vera Ho <vh71886@gmail.com>
2022-09-19 14:26:40 -0400

    better befitting-brackets

--- 7-stack/befitting-brackets.js
+++ 7-stack/befitting-brackets.js
@@ -20,3 +20,24 @@ const befittingBrackets = (str) => {
     return stack.length === 0;
 };    
 
+// Time complexity: O(n) linear
+// Space complexity: O(n) linear
+const befittingBracketsBetter = (str) => {
+    const stack = [];
+    const brackets = {
+      '(' : ')',
+      '[' : ']', 
+      '{' : '}'
+    };
+    
+    for(let char of str) {
+        if(char in brackets) stack.push(brackets[char]);
+        else {
+            let last = stack.length - 1;
+            if(stack[last] === char) stack.pop()
+            else return false;
+        }
+    }
+    
+    return stack.length === 0;
+};
