c6807d72d40129b414c68b448f0ffd521108dc25
Vera Ho <vh71886@gmail.com>
2022-09-27 16:33:01 -0400

    parenthetical-possibilities

---
+++ 8-exhaustive-recursion/parenthetical-possibilities.js
@@ -0,0 +1,28 @@
+// Time complexity: O(m^n) exponential where m is the length of longest str in () and n is the number of () sets
+// Space complexity: O(m^n) exponential
+const parentheticalPossibilities = (s) => {
+    if(!s.length) return [''];
+    let possibilities = [];
+    
+    if(s[0] === "(") {
+        let parenIndex = s.indexOf(')');
+        let parenChars = s.slice(1, parenIndex);  // chars within ()
+        let remainder = s.slice(parenIndex + 1);  // rest of the string after ()
+        let partialPoss = parentheticalPossibilities(remainder);
+        
+        // Loop through char in () and substrings (partials), adding each char to substrings
+        for(let i = 0; i < parenChars.length; i++) {
+            for(let j = 0; j < partialPoss.length; j++) {
+            possibilities.push(parenChars[i] + partialPoss[j]);
+            }
+        }
+    } else { // s[0] was not an '('
+        possibilities = parentheticalPossibilities(s.slice(1));
+        
+        for(let i = 0; i < possibilities.length; i++) {
+            possibilities[i] = s[0] + possibilities[i];
+        }
+    }
+    
+    return possibilities;
+};  
