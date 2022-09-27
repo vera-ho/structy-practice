7725cb74b8009e8b8719e66c4b0b4705560c2a00
Vera Ho <vh71886@gmail.com>
2022-09-27 16:50:12 -0400

    cleaner parenthetical-possibilities

--- 8-exhaustive-recursion/parenthetical-possibilities.js
+++ 8-exhaustive-recursion/parenthetical-possibilities.js
@@ -26,3 +26,38 @@ const parentheticalPossibilities = (s) => {
     
     return possibilities;
 };  
+
+
+// Time complexity: O(m^n) exponential where m is the length of longest str in () and n is the number of () sets
+// Space complexity: O(m^n) exponential
+const parentheticalPossibilitiesCleaner = (s) => {
+    if(!s.length) return [''];
+    
+    const { remainder, choices } = getChoices(s);
+    const partialPoss = parentheticalPossibilities(remainder);
+    let possibilities = [];
+    
+    for(let i = 0; i < choices.length; i++) {
+        for(let j = 0; j < partialPoss.length; j++) {
+            possibilities.push(choices[i] + partialPoss[j]);
+        }
+    }
+    
+    return possibilities;
+};
+  
+const getChoices = s => {
+    if(s[0] === '(') {
+        let endParenIdx = s.indexOf(')');
+        const choices = s.slice(1, endParenIdx).split('');
+        const remainder = s.slice(endParenIdx + 1);
+        
+        return { remainder, choices };
+    } else {
+        const remainder = s.slice(1);
+        const choices = [ s[0] ];
+        
+        return { remainder, choices }
+    }
+}
+  
