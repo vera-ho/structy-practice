efd103f013bba22a4a679c9cf8379ea2322ab1c5
Vera Ho <vh71886@gmail.com>
2022-09-30 11:37:41 -0400

    fast and slow pointer link traversal

--- 9-mixed-recall/middle-value.js
+++ 9-mixed-recall/middle-value.js
@@ -15,3 +15,21 @@
     
     return values[Math.floor(values.length / 2)];
 };
+
+/** Time complexity: O(n) linear
+ *  Space complexity: O(1) constant
+ *  Approach: 
+ *    - Use two pointers that move at different speeds through linked list
+ *    - Exit loop when the faster node is null or has no next node
+ */
+ const middleValueFaster = (head) => {
+    let slow = head, 
+        fast = head;
+    
+    while(fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+    return slow.val;
+};
