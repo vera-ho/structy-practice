b9427becbb69fa9c35d884e1ca4ec80fea78bcad
Vera Ho <vh71886@gmail.com>
2022-10-04 16:10:57 -0400

    lowest-common-ancestor

---
+++ 9-mixed-recall/lowest-common-ancestor.js
@@ -0,0 +1,40 @@
+/**
+ * Time complexity: O(n) linear
+ * Space complexity: O(n) linear
+ * Approach: Find the path to each value (DFS) and return the path in an array.
+ *   Put values for one path into a Set, and check the first occurrence in the other path.
+ */
+ const lowestCommonAncestor = (root, val1, val2) => {
+    const visited = new Set();
+    const path1 = findPath(root, val1, visited);
+    const path2 = findPath(root, val2);
+  
+    for(let val of path2) {
+      if(visited.has(val)) return val;
+    }
+  }
+  
+  const findPath = (node, val, visited = new Set()) => {
+    if(!node) return null;
+    if(node.val === val) {
+        visited.add(node.val);
+        return [ node.val ];
+    }
+  
+    let left = findPath(node.left, val, visited);
+    let right = findPath(node.right, val, visited);
+    
+    if(left) {
+        visited.add(node.val);
+        left.push(node.val);
+        return left;
+    }
+    
+    if(right) {
+        visited.add(node.val);
+        right.push(node.val)
+        return right;
+    } 
+    
+    return null;
+}
