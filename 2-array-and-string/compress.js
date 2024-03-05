b0f510aa2b956f7036415e9eb8e9fc3907709185
Vera Ho <vh71886@gmail.com>
2024-03-05 09:39:40 -0500

    compress

--- 2-array-and-string/compress.js
+++ 2-array-and-string/compress.js
@@ -19,9 +19,9 @@ const compress = (s) => {
       
         if(s[prev] != s[curr]) {
             if(count > 1) {
-            result.push(count, s[prev]);
+                result.push(count, s[prev]);
             } else {
-            result.push(s[prev]);
+                result.push(s[prev]);
             }
         
             prev = curr;
@@ -32,3 +32,26 @@ const compress = (s) => {
     
     return result.join("");
 };
+
+// time complexity: O(n) - runs through n characters in s
+// space complexity: O(n) only need storage for max n characters in s (if each character shows up just once)
+function compress2(s) {
+  const compressed = [];
+  let count = 0;
+  
+  for(let i = 0; i < s.length + 1; i++) {
+    const curr = s[i];
+    const prev = i > 0 ? s[i - 1] : s[i];
+    
+    if (curr === prev) {
+      count++;
+    } else if (count === 1) {
+      compressed.push(prev);
+    } else {
+      compressed.push(...[count, prev]);
+      count = 1;
+    }
+  }
+  
+  return compressed.join('');
+}
