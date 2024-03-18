9c9c5ae70a9bf6efaccaf07c6ba5324d1a399b91
Vera Ho <vh71886@gmail.com>
2024-03-18 13:27:36 -0400

    get node value

--- 3-linked-list/get-node-value.js
+++ 3-linked-list/get-node-value.js
@@ -29,3 +29,28 @@ const getNodeValueRecursive = (head, index) => {
     if(index === 0) return head.val
     else return getNodeValue(head.next, index - 1)
   };
+
+
+// Write a function, getNodeValue, that takes in the head of a linked list and an index. The function should return the value of the linked list at the specified index.
+// If there is no node at the given index, then return null.
+// approach
+// Iterate through the list until the index and return the value of that node. track progress using a counter
+// recursively: decrement index each time and return value when index = 0
+// time complexity: o(n) linear
+// space complexity: o(n) linear for recursive / o(1) constant for iterative
+const getNodeValue2Rec = (head, index) => {
+  if (!head) return null;
+  return !!index ? getNodeValue(head.next, index -1) : head.val;
+};
+
+const getNodeValue2 = (head, index) => {
+  let currentNode = head;
+  let counter = 0;
+  
+  while(counter != index && currentNode) {
+    currentNode = currentNode.next;
+    counter++;
+  }
+  
+  return currentNode?.val || null;
+};
