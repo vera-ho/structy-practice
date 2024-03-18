4c388b379a73923871f7abc3926f8daa6001d2e9
Vera Ho <vh71886@gmail.com>
2024-03-18 13:52:10 -0400

    reverse list

--- 3-linked-list/reverse-list.js
+++ 3-linked-list/reverse-list.js
@@ -32,3 +32,31 @@ const reverseListRecurseive = (head, prev = null) => {
     head.next = prev;
     return reverseList(next, head);
   };
+
+  // Write a function, reverseList, that takes in the head of a linked list as an argument. The function should reverse the order of the nodes in the linked list in-place and return the new head of the reversed linked list.
+// approach
+// track current, prev and next nodes and use them to flip the .next values for each nodes
+// iterate though the list flipping next values until the current node is null and return the prev node
+// recursively: when the end (head = null) is reached, return the prev node
+// time complexity: linear O(n)
+// space complexity: O(n) linear for recursive, O(1) constant for iterative
+const reverseList2 = (head) => {
+  let prev = null;
+  let current = head;
+  
+  while (current) {
+    let next = current.next;
+    current.next = prev; 
+    prev = current;
+    current = next;
+  }
+  
+  return prev;
+}
+
+const reverseList2Rec = (head, prev = null) => {
+  if (!head) return prev;
+  const next = head.next;
+  head.next = prev;
+  return reverseList(next, head)
+};
