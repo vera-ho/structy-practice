d980de2d371c22a39e9f96012b40fbfd668b6c7c
Vera Ho <vh71886@gmail.com>
2024-03-21 17:25:40 -0400

    longest streak

--- 3-linked-list/longest-streak.js
+++ 3-linked-list/longest-streak.js
@@ -36,3 +36,31 @@ const longestStreak2 = (head) => {
     }
     return maxCount;
 };
+
+// Write a function, longestStreak, that takes in the head of a linked list as an argument. The function should return the length of the longest consecutive streak of the same value within the list.
+// Approach
+// Track the streak value and max streak value and iterate through the entire list.
+// When the value changes, check if the streak surpoasses the max streak and if so, save it.
+// Reset streak to 1 (counting current value) and iterate pointer nodes.
+// Time complexity: linear O(n)
+// Space complexity: constant O(1)
+const longestStreak3 = (head) => {
+  let maxStreak = 0;
+  let streak = 0;
+  let current = head;
+  let prev = head;
+  
+  while (current) {
+    if (current.val === prev.val) {
+      streak++; 
+    } else {
+      streak = 1;
+    }
+    
+    if (streak > maxStreak) maxStreak = streak;
+    prev = current;
+    current = current.next;
+  }
+
+  return maxStreak;
+};
