1b7ab43a908312782327b5d600abeb06679bb4c2
Vera Ho <vh71886@gmail.com>
2024-03-21 15:42:02 -0400

    merge lists

--- 3-linked-list/merge-lists.js
+++ 3-linked-list/merge-lists.js
@@ -72,3 +72,53 @@ const mergeListsRecursive = (head1, head2) => {
   };
 
 mergeLists(a, q)
+
+// Write a function, mergeLists, that takes in the head of two sorted linked lists as arguments. The function should merge the two lists together into single sorted linked list. The function should return the head of the merged linked list.
+// Do this in-place, by mutating the original Nodes.
+// You may assume that both input lists are non-empty and contain increasing sorted numbers.
+
+// Approach: 
+// Values of nodes are numbers so we can easily compare values 
+// Use 2 pointers to track the current node for each list
+// Use a pointer to track the tail of the result
+// Decide which head to return by checking which is smaller and then increment tail and currentx
+// Loop until one or both node pointers are null
+// Time complexity: linear O(min(n, m))
+// Space complexity: constant O(1)
+const mergeLists2 = (head1, head2) => {
+  const head = head1.val > head2.val ? head2 : head1;
+  let tail = head;
+  let current1 = head1.val > head2.val ? head1 : head1.next; 
+  let current2 = head1.val > head2.val ? head2.next : head2;
+  
+  while (current1 && current2) {
+    if (current1.val > current2.val) {
+      tail.next = current2;
+      current2 = current2.next;
+    } else {
+      tail.next = current1;
+      current1 = current1.next;
+    }
+    tail = tail.next;
+  }
+  
+  tail.next = current1 ? current1 : current2;
+  
+  return head;
+};
+
+
+// Time complexity: linear O(min(n, m))
+// Space complexity: linear O(min(n, m)) - call stack
+const mergeLists2Rec = (head1, head2) => {
+  if (!head1 && !head2) return null;
+  if (!head1) return head2;
+  if (!head2) return head1;
+  
+  const head = head1.val > head2.val ? head2 : head1;
+  const next1 = head1.val > head2.val ? head1 : head1.next;
+  const next2 = head1.val > head2.val ? head2.next : head2;
+  head.next = mergeLists(next1, next2);
+
+  return head;
+};
