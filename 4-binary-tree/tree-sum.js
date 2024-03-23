624e7d6f990d99d9d773dd45dc00fb48c4ff1257
Vera Ho <vh71886@gmail.com>
2024-03-23 14:56:43 -0400

    tree sum

--- 4-binary-tree/tree-sum.js
+++ 4-binary-tree/tree-sum.js
@@ -44,3 +44,34 @@ const treeSumIterativeBFS = (root) => {
     
     return sum;
 };
+
+// Write a function, treeSum, that takes in the root of a binary tree that contains number values. The function should return the total sum of all values in the tree.
+// Approach: 
+// Use DFS or BFS to iterate through every node and add values to a running sum
+// Time complexity: linear O(n) for DFS and quadratic O(n^2) for BFS
+// Space complexity: linear O(n)
+const treeSum2 = (root) => {
+  if (!root) return 0;
+  
+  let sum = 0;
+  
+  // BFS
+  const queue = [root];
+  while (queue.length) {
+    const current = queue.shift();
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+    sum += current.val;
+  }
+  
+  // DFS
+//   const stack = [ root ];
+//   while (stack.length) {
+//     const current = stack.pop();
+//     if (current.left) stack.push(current.left);
+//     if (current.right) stack.push(current.right);
+//     sum += current.val;
+//   }
+  
+  return sum;
+};
