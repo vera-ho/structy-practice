10a18423155ea31bd03436c4ec92eb1daa3431b1
Vera Ho <vh71886@gmail.com>
2024-03-23 14:25:42 -0400

    dfs

--- 4-binary-tree/depth-first-values.js
+++ 4-binary-tree/depth-first-values.js
@@ -37,3 +37,45 @@ const depthFirstValuesRecursive = (root) => {
     return [root.val, ...left, ...right];
     // return [root.val].concat(left, right); // both work
 };
+
+// Write a function, depthFirstValues, that takes in the root of a binary tree. The function should return an array containing all values of the tree in depth-first order.
+// Approach: 
+// Depth first: Check all values going deep into the tree first to the leaves (i.e. left) before checking right nodes
+// Use a stack (FILO) to track progress - check if left and right nodes exist and add existing nodes to stack 
+// Add value to an array when the node is 'used' (not added to the stack)
+// Time complexity: linear O(n)
+// Space complexity: linear O(n)
+const depthFirstValues2 = (root) => {
+  if (!root) return [];
+  
+  // set arrays to hold stack of nodes and values
+  const stack = [ root ];
+  const values = []
+  
+  // iterate through all nodes in the binary tree
+  while(stack.length) {
+    // take the top node in stack and add any possible children to the stack
+    const node = stack.pop();
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+    
+    // save the value of the current node to the output array
+    values.push(node.val);
+  }
+  
+  return values;
+};
+
+// Time complexity: linear O(n)
+// Space complexity: linear O(n)
+const depthFirstValues2Rec = (root) => {
+  // if tree is empty, output is empty array
+  if (!root) return [];
+  
+  // if left/right nodes exist, run the fn with each node as its own 'root' and save the resulting array of values
+  const left = depthFirstValues(root.left);
+  const right = depthFirstValues(root.right);
+  
+  // concatentate root, left and right values together
+  return [root.val, ...left, ...right]
+};
