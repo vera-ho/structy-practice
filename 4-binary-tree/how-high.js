0b4971c17ea3d836ae2ab4fca59b291ae152c3b8
Vera Ho <vh71886@gmail.com>
2024-03-23 16:07:02 -0400

    how high

--- 4-binary-tree/how-high.js
+++ 4-binary-tree/how-high.js
@@ -8,3 +8,16 @@ const howHigh = (node) => {
     return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
 };
 
+// Write a function, howHigh, that takes in the root of a binary tree. The function should return a number representing the height of the tree.
+// The height of a binary tree is defined as the maximal number of edges from the root node to any leaf node.
+// If the tree is empty, return -1.
+
+// Approach: 
+// Use recursive DFS to find max height of left or right paths, keeping the value of the max height
+// Height is number of edges not number of nodes
+// Return the max path + self
+// Both complexities are linear O(n)
+const howHigh2 = (node) => {
+  if (!node) return -1;
+  return Math.max(howHigh(node.left), howHigh(node.right)) + 1;
+};
