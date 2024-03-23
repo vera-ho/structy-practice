288c5d1453e7f792aeeb6ab0827bd0ccc52eea2b
Vera Ho <vh71886@gmail.com>
2024-03-23 15:35:14 -0400

    maxpathsum

--- 4-binary-tree/max-root-to-leaf-path-sum.js
+++ 4-binary-tree/max-root-to-leaf-path-sum.js
@@ -10,3 +10,19 @@ const maxPathSum = (root) => {
     
     return root.val + Math.max(leftMax, rightMax);
 };
+
+// Write a function, maxPathSum, that takes in the root of a binary tree that contains number values. The function should return the maximum sum of any root to leaf path within the tree.
+// You may assume that the input tree is non-empty.
+// Approach: 
+// Use recursive DFS to calcuate sums of paths. Know node is a leaf if there are no children.
+// Count running sum starting with root (yay recursion) by taking the bigger sum on left or right and adding to self
+// Time complexity: linear O(n)
+// Space complexity: linear O(n)
+const maxPathSum2 = (root) => {
+  if (!root) return -Infinity; 
+  if (!root.left && !root.right) return root.val; // Leaf found
+  
+  const maxLeft = maxPathSum(root.left);
+  const maxRight = maxPathSum(root.right);
+  return maxLeft > maxRight ? root.val + maxLeft : root.val + maxRight
+};
