58e97e9474ab4c8f2dda230f0828be259ee21342
Vera Ho <vh71886@gmail.com>
2024-03-23 13:16:57 -0400

    add lists

--- 3-linked-list/add-lists.js
+++ 3-linked-list/add-lists.js
@@ -1,7 +1,7 @@
 // Time complexity: O(n) linear - O( max(n, m) )
 // Space complexity: O(n) linear - O( max(n, m) )
 // Recursive solution
-const addLists = (head1, head2, carry = 0) => {
+const addListsRecursive = (head1, head2, carry = 0) => {
     if(!head1 && !head2 && !carry) return null;
     
     const val1 = head1 ? head1.val : 0;
@@ -22,7 +22,7 @@ const addLists = (head1, head2, carry = 0) => {
 // Time complexity: O(n) linear - O( max(n, m) )
 // Space complexity: O(n) linear - O( max(n, m) )
 // Iterative solution
-const addListsRecursive = (head1, head2) => {
+const addLists = (head1, head2) => {
     let dummy = new Node(null);
     let tail = dummy;
     let current1 = head1;
@@ -46,3 +46,61 @@ const addListsRecursive = (head1, head2) => {
     
     return dummy.next;
   };
+
+
+// Write a function, addLists, that takes in the head of two linked lists, each representing a number. The nodes of the linked lists contain digits as values. The nodes in the input lists are reversed; this means that the least significant digit of the number is the head. The function should return the head of a new linked listed representing the sum of the input lists. The output list should have its digits reversed as well.
+// You must do this by traversing through the linked lists once.
+
+// Approach: 
+// Since the LSD is first in the lists, get the value of the nodes in both lists at the same position and add the values (including any carry)
+// If the sum is larger than 9, set a carry variable to 1 and set a new node to the singles digit of the sum
+// If one list is longer, add carry to the value of those nodes only
+
+// Time complexity: linear O(max(n, m)) 
+// Space complexity: linear O(max(n, m)) 
+const addLists2 = (head1, head2) => {
+  let carry = 0;
+  let current1 = head1;
+  let current2 = head2;
+  let dummyhead = new Node();
+  let tail = dummyhead;
+  
+  // Iterate until all nodes in both lists have been used
+  while(current1 || current2) {
+    // Calculate the sum, value for new node and updated carry
+    const digit1 = current1?.val || 0;
+    const digit2 = current2?.val || 0;
+    const sum = digit1 + digit2 + carry;
+    const nodeVal = sum > 9 ? sum % 10 : sum;
+    carry = sum > 9 ? 1 : 0;
+    
+    // Add the new node to the result list
+    tail.next = new Node(nodeVal);
+    
+    // Update pointers
+    current1 = current1?.next;
+    current2 = current2?.next;
+    tail = tail.next;
+  }
+  
+  // If there's a carry left over, add the final node
+  tail.next = carry ? new Node(carry) : null;
+  
+  return dummyhead.next;
+};
+
+// Time complexity: linear O(max(n, m)) 
+// Space complexity: linear O(max(n, m)) 
+// Each recursive step returns a node for the solution
+const addLists2Rec = (head1, head2, carry = 0) => {
+  if (!head1 && !head2 && !carry) return null;
+  
+  const digit1 = head1 ? head1.val : 0;
+  const digit2 = head2 ? head2.val : 0;
+  const sum = digit1 + digit2 + carry;
+  carry = sum > 9 ? 1 : 0;
+  
+  const newNode = new Node(sum % 10);
+  newNode.next = addLists(head1?.next, head2?.next, carry);
+  return newNode;
+};
