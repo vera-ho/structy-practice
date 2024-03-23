de685b76b2df1427d8c7cb7f08c04cf0f1cc99f7
Vera Ho <vh71886@gmail.com>
2024-03-23 15:58:20 -0400

    tree value count

--- 4-binary-tree/tree-value-count.js
+++ 4-binary-tree/tree-value-count.js
@@ -42,3 +42,34 @@ const treeValueCountBFSIterative = (root, target) => {
     
     return count;
 };
+
+// Write a function, treeValueCount, that takes in the root of a binary tree and a target value. The function should return the number of times that the target occurs in the tree.
+// Approach: Iterate through list using DFS or BFS and add to a counter every time the target is found
+// Time complexity: DFS O(n) and BFS O(n^2)
+// Space complexity: O(n)
+const treeValueCount2 = (root, target) => {
+  if (!root) return 0;
+  const stack = [root];
+  let count = 0;
+  
+  // For BFS, shift instead of pop from a queue
+  while (stack.length) {
+    const current = stack.pop();
+    if (current.left) stack.push(current.left);
+    if (current.right) stack.push(current.right);
+    if (current.val === target) count++;
+  }
+  
+  return count;
+};
+
+const treeValueCount2Rec = (root, target) => {
+  if (!root) return 0;
+  const leftCount = treeValueCount(root.left, target);
+  const rightCount = treeValueCount(root.right, target);
+  return leftCount + rightCount + (root.val === target ? 1 : 0);
+};
+
+const treeValueCountRecWhy = (root, target) => {
+  return !root ? 0 : ((treeValueCount(root.left, target)) + (treeValueCount(root.right, target)) + ((root.val === target ? 1 : 0)))
+};
