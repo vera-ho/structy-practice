9a0b94f42d704363dd9a84a910a9d2d5136e827f
Vera Ho <vh71886@gmail.com>
2024-03-24 11:01:37 -0400

    tree levels

--- 4-binary-tree/tree-levels.js
+++ 4-binary-tree/tree-levels.js
@@ -33,25 +33,41 @@ const treeLevels = (root) => {
 // Space complexity: O(n) linear
 // Recursive DFS solution
 const treeLevelsRecursive = (root) => {
-    if(!root) return [];
+  if(!root) return [];
     if(!root.left && !root.right) return [[root.val]]
     
+    // treeValues[0] is like the 'root' of this recursice step
+    // get the values from other levels in 2D array format
     const treeValues = [[root.val]];
     const left = treeLevels(root.left);
     const right = treeLevels(root.right);
     
+    // Check each element of the left result which are at levels below current 'root'
+    // Check if an array already exists for the level and either create the array at the index or concatenate values to it
     for(let i = 0; i < left.length; i++) {
         if(treeValues[i + 1]) treeValues[i + 1] = treeValues[i + 1].concat(left[i]);
         else treeValues[i + 1] = left[i];
+        // console.log('left', i, treeValues)
     }
     
+    // do the same for the right side results
     for(let i = 0; i < right.length; i++) {
         if(treeValues[i + 1]) treeValues[i + 1] = treeValues[i + 1].concat(right[i]);
         else treeValues[i + 1] = right[i];
+        // console.log('right', i, treeValues)
     }
     
-    return treeValues;
+  return treeValues;
 };
+
+// Console log results ^^
+// left 0 [ [ 'b' ], [ 'd' ] ] 
+// right 0 [ [ 'b' ], [ 'd', 'e' ] ] 
+// right 0 [ [ 'c' ], [ 'f' ] ] 
+// left 0 [ [ 'a' ], [ 'b' ] ] 
+// left 1 [ [ 'a' ], [ 'b' ], [ 'd', 'e' ] ] 
+// right 0 [ [ 'a' ], [ 'b', 'c' ], [ 'd', 'e' ] ] 
+// right 1 [ [ 'a' ], [ 'b', 'c' ], [ 'd', 'e', 'f' ] ] 
 
 // Time complexity: O(n) linear
 // Space complexity: O(n) linear
@@ -126,3 +142,56 @@ const treeLevelsRecursive2 = (root) => {
     return treeValues;
 };
 
+
+// Write a function, treeLevels, that takes in the root of a binary tree. The function should return a 2-Dimensional array where each subarray represents a level of the tree.
+
+// Approach: 
+// Track node value and level of depth 
+// For each node, check if that level exists - if so, add value to it otherwise, create it (levels index)
+
+// Time complexity: O(n) linear
+// Space complexity: O(n) linear
+const treeLevels2 = (root) => {
+  if (!root) return [];
+
+  const stack = [[root, 0]]; // [[node, level]]
+  const levels = [];
+  
+  while (stack.length) {
+    const [current, level] = stack.pop();
+    if (current.right) stack.push([current.right, level + 1]);
+    if (current.left) stack.push([current.left, level + 1]);
+    if (levels[level]) {
+      levels[level].push(current.val)
+    } else {
+      levels[level] = [current.val];
+    }
+  }
+  
+  return levels;
+};
+
+// Recursive approach
+// Track level and output array
+// For each node, add itself to the proper place and then run the fn on left and right children, if any
+// Time and space complexities are both linear O(n)
+const treeLevels2Rec = (root, level = 0, levels = []) => {
+  if (!root) return [];
+  
+  if (levels[level]) levels[level].push(root.val)
+  else levels[level] = [root.val]
+//   console.log('level', level, levels)
+  
+  const left = treeLevels(root.left, level + 1, levels);
+  const right = treeLevels(root.right, level + 1, levels);
+  
+  return levels;
+};
+
+// Log output
+// level 0 [ [ 'a' ] ] 
+// level 1 [ [ 'a' ], [ 'b' ] ] 
+// level 2 [ [ 'a' ], [ 'b' ], [ 'd' ] ] 
+// level 2 [ [ 'a' ], [ 'b' ], [ 'd', 'e' ] ] 
+// level 1 [ [ 'a' ], [ 'b', 'c' ], [ 'd', 'e' ] ] 
+// level 2 [ [ 'a' ], [ 'b', 'c' ], [ 'd', 'e', 'f' ] ] 
