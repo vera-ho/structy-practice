d711ce673b3128d8796a27fbfcbefaff14fcaf07
Vera Ho <vh71886@gmail.com>
2024-03-26 18:06:30 -0400

    longest path

--- 5-graph/longest-path.js
+++ 5-graph/longest-path.js
@@ -54,3 +54,68 @@ const longestPathTracked = (graph) => {
     pathLengths[node] = max + 1;
     return pathLengths[node];
 }
+
+
+// ------------------------------------------------------------------------- //
+
+// Write a function, longestPath, that takes in an adjacency list for a directed acyclic graph. The function should return the length of the longest path within the graph. A path may start and end at any two nodes. The length of a path is considered the number of edges in the path, not the number of nodes.
+
+// Approach: 
+// Use DFS to reach to each furthest path and return the path count
+// Compare and keep longest path
+// Check every starting node in the adjacency list
+// Save paths calculated from a node so we don't have to recalculate it if we encounter it again
+
+// Complexity: linear O(n) for space to store visited distances and linear O(e) for time
+const longestPath2 = (graph) => {
+  let maxDistance = 0;
+  let visitedDistance = {}; // stores longest path from a previously calculated node
+  
+  for (let node in graph) {
+    const distance = checkPath(graph, node, visitedDistance);
+    if (distance > maxDistance) maxDistance = distance;
+  }
+  
+  return maxDistance;
+};
+
+const checkPath = (graph, node, visitedDistance) => {
+  if (node in visitedDistance) return visitedDistance[node];
+  if (!graph[node].length) return 0;
+  let maxDistance = 0;
+  
+  for (let neighbor of graph[node]) {
+    const distance = checkPath(graph, neighbor, visitedDistance) + 1;
+    if (distance > maxDistance) maxDistance = distance;
+  }
+  
+  visitedDistance[node] = maxDistance;
+  return maxDistance;
+}
+
+  // Iterative fails due to timeout for large graphs ***** not logically possible to do it iterativelly?
+  //   const stack = [[node, 0]]; // [node, distance]
+  //   let maxDistance = 0;
+
+  //   while(stack.length) {
+  //     let [current, distance] = stack.pop();
+
+  //     for (let neighbor of graph[current]) {
+  //       if (neighbor in visitedDistance) {
+  //         visitedDistance[current] = visitedDistance[neighbor] + 1;
+  //         maxDistance = visitedDistance[current];
+  //       } else {
+  //         stack.push([neighbor, distance + 1]);
+  //       }
+  //     }
+
+  //     if (distance > maxDistance) maxDistance = distance;
+  //   }
+
+  //   // Save calculated maximum distance from this node
+  //   visitedDistance[node] = maxDistance;
+  //   return maxDistance;
+
+module.exports = {
+  longestPath,
+};
