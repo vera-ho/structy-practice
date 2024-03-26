ad2440da095b5eb79fb848304b467cada538a3c9
Vera Ho <vh71886@gmail.com>
2024-03-26 14:19:10 -0400

    island count

--- 5-graph/island-count.js
+++ 5-graph/island-count.js
@@ -39,3 +39,80 @@ const exploreIsland = (grid, row, col, visited) => {
         exploreIsland(grid, newRow, newCol, visited);
     }
 };
+
+// ------------------------------------------------------------------------- //
+// Write a function, islandCount, that takes in a grid containing Ws and Ls. W represents water and L represents land. The function should return the number of islands on the grid. An island is a vertically or horizontally connected region of land.
+
+// Approach: 
+// Track visited using a Set
+// "neighbors" are +/- 1 from row or column
+// Iterate though 2D array until land is found (that hasnt been previously visited)
+// Traverse island until all of it has been visited and there is nothing left to traverse
+// Add to island count and continue to iterate though 2D array
+
+// Time complexity: 
+// Space complexity: 
+const islandCount2 = (grid) => {
+  const visited = new Set();
+  let count = 0;
+  
+  // iterate through the grid, cell by cell
+  for(let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      // start island traversal if location is land and not previously visited
+      if(grid[row][col] === "L" && !visited.has(row + 'x' + col)) {
+        // expore the island to add mapping to visited
+        exploreIsland(grid, row, col, visited);
+        count++;
+      }
+    }
+  }
+  
+  return count;
+};
+
+// Possible directions from a cell (up down left right)
+const directions = [
+  [0, +1], // right
+  [0, -1], // left
+  [+1, 0], // up
+  [-1, 0]  // down
+]
+
+// Return 2D array of in-grid coordinates for current land position
+// Does not check if neighbor is land or water
+const getNeighbors = (grid, row, col) => {
+  const neighbors = [];
+  for (const direction of directions) {
+    const [x, y] = direction;
+    const nextCoord = [row + x, col + y];
+    
+    // Check grid bounds
+    const rowMax = grid.length;
+    const colMax = grid[0].length;
+    if (nextCoord[0] < rowMax && nextCoord[0] >=0 && nextCoord[1] < colMax && nextCoord[1] >=0) {
+      neighbors.push(nextCoord)
+    }
+  }
+  
+  return neighbors;
+}
+
+// Recursively traverse the island
+const exploreIsland2 = (grid, row, col, visited) => {
+  if (visited.has(row + 'x' + col)) return;
+  if (grid[row][col] === "W") return;
+  
+  // map the current land position
+  visited.add(row + 'x' + col);
+  const neighbors = getNeighbors(grid, row, col);
+  
+  for (const coordinates of neighbors) {
+    const [nextRow, nextCol] = coordinates;
+    
+    // continue to explore unmapped land
+    if (!visited.has(nextRow + 'x' + nextCol)) {
+      exploreIsland(grid, nextRow, nextCol, visited);
+    }
+  }
+};
--- 5-graph/shortest-path.js
+++ 5-graph/shortest-path.js
@@ -39,3 +39,49 @@ const convertToGraph = (edges) => {
   
     return graph;
 }
+
+// Write a function, shortestPath, that takes in an array of edges for an undirected graph and two nodes (nodeA, nodeB). The function should return the length of the shortest path between A and B. Consider the length as the number of edges in the path, not the number of nodes. If there is no path between A and B, then return -1.
+
+// Approach: 
+// Track visited nodes with a set and use queue to track distance from source node
+// BFS would give shortest path when the node is seen for the first time
+
+// Time complexity: linear O(e)
+// Space complexity: linear O(n)
+const shortestPath2 = (edges, nodeA, nodeB) => {
+  const graph = edgesToGraph(edges);
+  return traversePath(graph, nodeA, nodeB, new Set());
+};
+
+const traversePath = (graph, src, dest, visited) => {
+  const queue = [[src, 0]];
+  
+  while(queue.length) {
+    const [node, distance] = queue.shift();
+    if(node === dest) return distance;
+    visited.add(node);
+    
+    for (const neighbor of graph[node]) {
+      if (!visited.has(neighbor)) {
+        queue.push([neighbor, distance + 1])
+      }
+    }
+  }
+  
+  return -1;
+}
+
+// Convert edges to undirected graph's adjacency list
+const edgesToGraph = edges => {
+  const graph = {};
+  
+  for (const edge of edges) {
+    const [nodeA, nodeB] = edge;
+    if (!graph[nodeA]) graph[nodeA] = [];
+    if (!graph[nodeB]) graph[nodeB] = [];
+    graph[nodeA].push(nodeB);
+    graph[nodeB].push(nodeA);
+  }
+  
+  return graph;
+}
