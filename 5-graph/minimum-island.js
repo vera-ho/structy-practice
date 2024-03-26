dfbc30e00c33f31e7738c2fff99de36d3826eebb
Vera Ho <vh71886@gmail.com>
2024-03-26 14:53:23 -0400

    minimum island

--- 5-graph/minimum-island.js
+++ 5-graph/minimum-island.js
@@ -41,3 +41,80 @@ const exploreIsland = (grid, row, col, visited) => {
     
     return size;
 }
+
+// ------------------------------------------------------------------------- //
+
+// Write a function, minimumIsland, that takes in a grid containing Ws and Ls. W represents water and L represents land. The function should return the size of the smallest island. An island is a vertically or horizontally connected region of land.
+// You may assume that the grid contains at least one island.
+
+// Approach: 
+// Iterate though grid and look for land. Traverse through the island and count how many L's are found
+// Track visited L & W with a Set
+// Compare each island size with the smallest found (so far) and update if its smaller
+const minimumIsland2 = (grid) => {
+  const visited = new Set();
+  let smallestIsland = Infinity;
+  
+  // Iterate through the grid and traverse each island. Compare island sizes and save smallest island size each time
+  for(let row = 0; row < grid.length; row++){
+    for(let col = 0; col < grid[0].length; col++) {
+      if (grid[row][col] === "W") {
+        visited.add(row + ' ' + col);
+        continue;
+      }
+      
+      // Unmapped land is found so explore and compare sizes
+      if (!visited.has(row + ' ' + col) && grid[row][col] === "L") {
+        const islandSize = exploreIsland(grid, row, col, visited);
+        if (islandSize < smallestIsland) smallestIsland = islandSize;
+      }
+    }
+  }
+  
+  return smallestIsland;
+};
+
+// Previously visited or water contribute 0 to island size
+const exploreIsland2 = (grid, row, col, visited) => {
+  if (visited.has(row + ' ' + col)) return 0;
+  visited.add(row + ' ' + col);
+  if (grid[row][col] === "W") return 0;
+  
+  const neighbors = getNeighbors(grid, row, col);
+  let size = 1; // including self
+  
+  for (let neighbor of neighbors) {
+    const [nextRow, nextCol] = neighbor;
+    size += exploreIsland(grid, nextRow, nextCol, visited);
+  }
+  
+  return size;
+};
+
+// Possible directions from a cell (up down left right)
+const directions = [
+  [0, +1], // right
+  [0, -1], // left
+  [+1, 0], // up
+  [-1, 0]  // down
+];
+
+// Return 2D array of in-grid coordinates for current land position (helper)
+// Does not check if neighbor is land or water
+const getNeighbors = (grid, row, col) => {
+  const neighbors = [];
+  
+  for (const direction of directions) {
+    const [x, y] = direction;
+    const nextCoord = [row + x, col + y];
+    
+    // Check grid bounds
+    const rowMax = grid.length;
+    const colMax = grid[0].length;
+    if (nextCoord[0] < rowMax && nextCoord[0] >=0 && nextCoord[1] < colMax && nextCoord[1] >=0) {
+      neighbors.push(nextCoord)
+    }
+  }
+  
+  return neighbors;
+};
