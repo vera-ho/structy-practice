58d755986584048c52867a1eb9b69a38495d7c07
Vera Ho <vh71886@gmail.com>
2024-03-26 12:00:55 -0400

    connected components count

--- 5-graph/connected-components-count.js
+++ 5-graph/connected-components-count.js
@@ -23,3 +23,50 @@ const traverseNode = (graph, node, visited) => {
     }
     return true;
 }
+
+// Write a function, connectedComponentsCount, that takes in the adjacency list of an undirected graph. The function should return the number of connected components within the graph.
+
+// Approach: 
+// Track components using a counter and visited nodes using a Set
+// Traverse through entire adjacency list's nodes
+// When the traversal can't go any further, add to counter
+
+// Time complexity: O(e) linear
+// Space complexity: O(n) linear
+const connectedComponentsCount2 = (graph) => {
+  let count = 0;
+  const visited = new Set();
+  
+  for(const node in graph) {
+    if (!visited.has(String(node))) {
+      traverse(graph, node, visited);
+      count++;
+    }
+  }
+  
+  return count;
+};
+
+// Traverse all paths starting from this node and add traversed nodes to visited
+const traverse = (graph, node, visited) => {  
+  // Recursive DFS
+  // if(visited.has(node)) return;
+  // visited.add(String(node));
+  
+  // for (const neighbor of graph[node]) {
+  //   if (!visited.has(String(neighbor))) {
+  //     traverse(graph, neighbor, visited)
+  //   }   
+  // }
+  
+  // Iterative BFS
+  const queue = [node];
+  
+  while (queue.length) {
+    const current = queue.shift();
+    visited.add(String(current))
+    for (const neighbor of graph[current]) {
+      if (!visited.has(String(neighbor))) queue.push(neighbor)
+    }
+  }
+}
