15daf03aa5ff83e761a6e3e2fdd55e56938b8e3b
Vera Ho <vh71886@gmail.com>
2024-03-27 12:29:53 -0400

    min change

--- 6-dynamic-programming/min-change.js
+++ 6-dynamic-programming/min-change.js
@@ -18,3 +18,33 @@ const minChange = (amount, coins, coinPurse = {}) => {
     if(!(amount in coinPurse)) coinPurse[amount] = -1;
     return coinPurse[amount];
 };
+
+// Write a function minChange that takes in an amount and an array of coins. The function should return the minimum number of coins required to create the amount. You may use each coin as many times as necessary.
+// If it is not possible to create the amount, then return -1.
+
+// Approach: 
+// Base case amount = 0 -> 0 levels
+// Base case amount < 0 returns -1 for not possible
+// Each level above that can add 1 to level count
+// Use memo to store number of levels to get to 0
+// For each amount, iterate through the coins and subtract the value; run minChange on new value
+// Save results to memo
+
+// TC: linear O(a * c)
+// SC: linear O(a) 
+const minChange2 = (amount, coins, memo = {}) => {
+  if (amount < 0) return -1;
+  if (amount === 0) return 0;
+  if (amount in memo) return memo[amount];
+  
+  memo[amount] = Infinity;
+  for (const value of coins) {
+    const levels = minChange(amount - value, coins, memo);
+    if (levels === -1) continue;
+    if (levels < memo[amount]) memo[amount] = levels + 1;
+  }
+
+  // if the result was never set to something other than infinity, then it was not possible for this amount
+  memo[amount] = memo[amount] === Infinity ? -1 : memo[amount];
+  return memo[amount]
+};
