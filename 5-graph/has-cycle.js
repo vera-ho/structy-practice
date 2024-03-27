356ae8671c8b0d1bc679b1e2de38d2e2e62b6a3e
Vera Ho <vh71886@gmail.com>
2024-03-27 10:57:53 -0400

    has cycle

--- 5-graph/has-cycle.js
+++ 5-graph/has-cycle.js
@@ -24,3 +24,43 @@ const detectCycle = (graph, node, visiting, visited) => {
     visited.add(node);
     return false;
 };
+
+// Write a function, hasCycle, that takes in an object representing the adjacency list of a directed graph. The function should return a boolean indicating whether or not the graph contains a cycle.
+
+// Approach: 
+// Traverse every node in graph and store visited nodes
+// white-grey-black algo for cycle detection
+// white - unexplored / grey - in progress / black - fully visited
+
+// Time complexity: linear O(e)
+// Space complexity: linear O(n)
+const hasCycle2 = (graph) => {
+  const grey = new Set();
+  const black = new Set();
+    
+  for (let node in graph) {
+    const hasCycle = explore(graph, node, grey, black);
+    if (hasCycle) return true;
+  }
+  return false;
+};
+
+const explore = (graph, node, grey, black) => {
+  if (grey.has(node)) return true;
+  if (black.has(node)) return false;
+  
+  // Node is unexplored and can move it to in-progress now
+  grey.add(node);
+  for (let nextNode of graph[node]) {
+    const hasCycle = explore(graph, nextNode, grey, black);
+    if (hasCycle) return true;
+  }
+  
+  grey.delete(node);
+  black.add(node);
+  return false;
+}
+
+module.exports = {
+  hasCycle,
+};
