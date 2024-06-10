27b0fa49ef26aa8d44dea42dd4de3a93ec3198be
Vera Ho <vh71886@gmail.com>
2024-06-10 11:36:09 -0400

    fix complexity

--- 2-beginner-recursion/sum-of-lengths.js
+++ 2-beginner-recursion/sum-of-lengths.js
@@ -7,8 +7,8 @@
 // Base case is an array length of zero, returning string count of zero
 // Return string count plus result from function call
 
-// TC: O(n) linear
-// SC: O(n) linear due to stack
+// TC: O(n^2) quadratic
+// SC: O(n^2) quadratic
 const sumOfLengths = (strings) => {
   if(strings.length === 0) return 0;
   const [current, ...otherStrings] = strings;
