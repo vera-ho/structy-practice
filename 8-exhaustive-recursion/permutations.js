b5aa52259de03b6143ce9d1f60c37a80c816235d
Vera Ho <vh71886@gmail.com>
2024-07-21 15:25:07 -0400

    permutations

--- 8-exhaustive-recursion/permutations.js
+++ 8-exhaustive-recursion/permutations.js
@@ -1,18 +1,73 @@
-// Time complexity: O(n!) factorial - worst complexity!
+// Write a function, permutations, that takes in an array an argument. The function should return a 2D array where each subarray represents one of the possible permutations of the array.
+// The subarrays may be returned in any order.
+// You may assume that the input array contains unique elements.
+
+// Approach
+// Break the problem down by taking the first element away and running permutations on the rest of the array until the base case of empty items
+// For each result of the recursive stack, add the removed first item to each index of the result arrays
+
+// Time complexity: O(n!) factorial
 // Space complexity: O(n!) factorial
 const permutations = (items) => {
-    if(!items.length) return [[]];
-    
-    let first = items[0];
-    let subPerm = permutations(items.slice(1));
-    
-    let withFirst = [];
-    for(let i = 0; i < subPerm.length; i++) {
-        for(let j = 0; j <= subPerm[i].length; j++) {
-            withFirst.push([...subPerm[i].slice(0, j) , first , ...subPerm[i].slice(j)]);
-        }
+  if (items.length === 0) return [[]];
+
+  const first = items[0];
+  const subPerms = permutations(items.slice(1));
+  const fullPerm = [];
+
+  for (let perm of subPerms) {
+    for (let i = 0; i <= perm.length; i++) {
+      fullPerm.push([...perm.slice(0, i), first, ...perm.slice(i)])
     }
-    
-    return withFirst;
+  }
+
+  return fullPerm;
 };
-  
+
+// permutations(['a', 'b', 'c']); // -> 
+// [ 
+//   [ 'a', 'b', 'c' ], 
+//   [ 'b', 'a', 'c' ], 
+//   [ 'b', 'c', 'a' ], 
+//   [ 'a', 'c', 'b' ], 
+//   [ 'c', 'a', 'b' ], 
+//   [ 'c', 'b', 'a' ] 
+// ] 
+
+// permutations(['red', 'blue']); // ->
+// [ 
+//   [ 'red', 'blue' ], 
+//   [ 'blue', 'red' ] 
+// ]
+
+// permutations([8, 2, 1, 4]); // ->
+// [ 
+//   [ 8, 2, 1, 4 ], [ 2, 8, 1, 4 ], 
+//   [ 2, 1, 8, 4 ], [ 2, 1, 4, 8 ], 
+//   [ 8, 1, 2, 4 ], [ 1, 8, 2, 4 ], 
+//   [ 1, 2, 8, 4 ], [ 1, 2, 4, 8 ], 
+//   [ 8, 1, 4, 2 ], [ 1, 8, 4, 2 ], 
+//   [ 1, 4, 8, 2 ], [ 1, 4, 2, 8 ], 
+//   [ 8, 2, 4, 1 ], [ 2, 8, 4, 1 ], 
+//   [ 2, 4, 8, 1 ], [ 2, 4, 1, 8 ], 
+//   [ 8, 4, 2, 1 ], [ 4, 8, 2, 1 ], 
+//   [ 4, 2, 8, 1 ], [ 4, 2, 1, 8 ], 
+//   [ 8, 4, 1, 2 ], [ 4, 8, 1, 2 ], 
+//   [ 4, 1, 8, 2 ], [ 4, 1, 2, 8 ] 
+// ] 
+
+// permutations([8, 2, 1, 4]); // ->
+// [ 
+//   [ 8, 2, 1, 4 ], [ 2, 8, 1, 4 ], 
+//   [ 2, 1, 8, 4 ], [ 2, 1, 4, 8 ], 
+//   [ 8, 1, 2, 4 ], [ 1, 8, 2, 4 ], 
+//   [ 1, 2, 8, 4 ], [ 1, 2, 4, 8 ], 
+//   [ 8, 1, 4, 2 ], [ 1, 8, 4, 2 ], 
+//   [ 1, 4, 8, 2 ], [ 1, 4, 2, 8 ], 
+//   [ 8, 2, 4, 1 ], [ 2, 8, 4, 1 ], 
+//   [ 2, 4, 8, 1 ], [ 2, 4, 1, 8 ], 
+//   [ 8, 4, 2, 1 ], [ 4, 8, 2, 1 ], 
+//   [ 4, 2, 8, 1 ], [ 4, 2, 1, 8 ], 
+//   [ 8, 4, 1, 2 ], [ 4, 8, 1, 2 ], 
+//   [ 4, 1, 8, 2 ], [ 4, 1, 2, 8 ] 
+// ] 
