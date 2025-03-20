fac082fc8b2541243745aa2e85e57c987a6321ca
Vera Ho <vh71886@gmail.com>
2025-03-20 10:54:49 -0400

    all-unique better

--- 1-array-and-string/all-unique.js
+++ 1-array-and-string/all-unique.js
@@ -1,23 +1,15 @@
 // Write a function, allUnique, that takes in an array. The function should return a boolean indicating whether or not the array contains unique items.
 
 // Approach
-// Iterate through the array and create a key value pair for each element
-// key is the element and value is the count initialized to 1
-// If the key already exists, return false since there is a duplicate
+// Create a set from items to store only unique elements of items
+// Compare the size of the set and the item array length
+// If the array contains all unique items, the sizes will be the same
 
 // Timce complexity: O(n) linear
 // Space complexity: O(n) linear
 const allUnique = (items) => {
-    const counter = {};
-
-    for (let item of items) {
-        if (counter[item]) {
-            return false;
-        }
-        counter[item] = 1;
-    }
-
-    return true;
+    const uniqueSet = new Set(items);
+    return uniqueSet.size === items.length;
 };
 
 // allUnique(["q", "r", "s", "a"]); // -> true
